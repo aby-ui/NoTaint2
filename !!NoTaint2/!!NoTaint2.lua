cfef8b68115321a7f59260bd78911b06e2a1a9c3
aby-ui <aby_ui@163.com>
2022-11-02 12:01:39 +0800

    r221031

---
+++ !!NoTaint2/!!NoTaint2.lua
@@ -0,0 +1,86 @@
+if NoTaint2_CleanStaticPopups then return end
+
+if GetLocale() == "zhCN" then
+    C_Timer.After(1, function()
+        print("|cff00ff00!!NoTaint2|r-改善动作条污染，由|cffffff00warbaby-abyUI(爱不易)|r提供")
+    end)
+end
+
+--code since !NoTaint, test: /run StaticPopup_Show('PARTY_INVITE',"a") /dump issecurevariable(StaticPopup1, "which")
+function NoTaint2_CleanStaticPopups()
+    for index = 1, STATICPOPUP_NUMDIALOGS, 1 do
+        local frame = _G["StaticPopup"..index];
+        if not issecurevariable(frame, "which") then
+            if frame:IsShown() then
+                local info = StaticPopupDialogs[frame.which];
+                if info and not issecurevariable(info, "OnCancel") then
+                    info.OnCancel()
+                end
+                frame:Hide()
+            end
+            frame.which = nil
+        end
+    end
+end
+
+function NoTaint2_CleanDropDownList()
+    local frameToShow = LFDQueueFrameTypeDropDown
+    local parent = frameToShow:GetParent()
+    frameToShow:SetParent(nil) --to show
+    --RequestLFDPlayerLockInfo() --to trigger LFG_LOCK_INFO_RECEIVED
+    frameToShow:SetParent(parent)
+end
+
+local global_obj_name = {
+    UIDROPDOWNMENU_MAXBUTTONS = 1,
+    UIDROPDOWNMENU_MAXLEVELS = 1,
+    UIDROPDOWNMENU_OPEN_MENU = 1,
+    UIDROPDOWNMENU_INIT_MENU = 1,
+    OBJECTIVE_TRACKER_UPDATE_REASON = 1,
+}
+
+function NoTaint2_CleanGlobal(self)
+    for k, _ in pairs(global_obj_name) do
+        if not issecurevariable(k) then
+            --print("clean", k, issecurevariable(k))
+            _G[k] = nil
+        end
+    end
+end
+
+hooksecurefunc(EditModeManagerFrame, "ClearActiveChangesFlags", function(self)
+    for _, systemFrame in ipairs(self.registeredSystemFrames) do
+        systemFrame:SetHasActiveChanges(nil);
+    end
+    self:SetHasActiveChanges(nil);
+end)
+
+hooksecurefunc(EditModeManagerFrame, "IsEditModeLocked", function()
+    NoTaint2_CleanGlobal()
+end)
+
+local function cleanAll()
+    if not issecurevariable(DropDownList1, "numButtons") or not issecurevariable("UIDROPDOWNMENU_MAXLEVELS") then
+        print("!!NoTaint2 works!")
+    end
+    NoTaint2_CleanDropDownList()
+    NoTaint2_CleanStaticPopups()
+    NoTaint2_CleanGlobal()
+end
+
+local Origin_IsShown = EditModeManagerFrame.IsShown
+hooksecurefunc(EditModeManagerFrame, "IsShown", function(self)
+    if Origin_IsShown(self) then return end
+    local stack = debugstack(4)
+    --call from UIParent.lua if ( not frame or frame:IsShown() ) then
+    --different when hooked
+    if stack:find('[string "=[C]"]: in function `ShowUIPanel\'\n', 1, true) then
+        cleanAll()
+    end
+end)
+
+-- In case the stack check is failed, assure the game menu entrance.
+-- Running cleanAll() multi times has no side effects.
+GameMenuButtonEditMode:HookScript("PreClick", function()
+    cleanAll()
+end)
